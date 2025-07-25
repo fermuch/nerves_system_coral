784eaf8c58fc24001dc8208e3a1ac99d2faf6a53
Fernando Mumbach <fermuch@ayvu.net>
2025-07-25 14:37:43 -0300

    reverting everything to the minimum possible working image

--- mix.exs
+++ mix.exs
@@ -62,7 +62,7 @@ defmodule NervesSystemCoral.MixProject do
   defp deps do
     [
       {:nerves, "~> 1.11", runtime: false},
-      {:nerves_system_br, "1.28.2", runtime: false},
+      {:nerves_system_br, github: "nerves-project/nerves_system_br", tag: "v1.32.0", runtime: false},
       {:nerves_toolchain_aarch64_nerves_linux_gnu, "~> 13.2.0", runtime: false},
       {:nerves_system_linter, "~> 0.4", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.34", only: :docs, runtime: false}
