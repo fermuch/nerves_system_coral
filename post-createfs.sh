1114ea608d351e17701e7a7c4197680117e8c010
Fernando Mumbach <fermuch@ayvu.net>
2025-07-28 13:30:09 -0300

    configuring so it generates and embeds the env file for uboot

--- post-createfs.sh
+++ post-createfs.sh
@@ -2,7 +2,10 @@
 
 set -ex
 
-# Run the common post-image processing for nerves
-$BR2_EXTERNAL_NERVES_PATH/board/nerves-common/post-createfs.sh \
-  $TARGET_DIR \
-  $NERVES_DEFCONFIG_DIR/fwup.conf
+cp ${BINARIES_DIR}/uboot-env.bin ${BOOTFATDIR}/uboot-env.bin
+
+
+# # Run the common post-image processing for nerves
+# $BR2_EXTERNAL_NERVES_PATH/board/nerves-common/post-createfs.sh \
+#   $TARGET_DIR \
+#   $NERVES_DEFCONFIG_DIR/fwup.conf
