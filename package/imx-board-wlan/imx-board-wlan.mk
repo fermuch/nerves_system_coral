6cfca88e4acade06e22267e5bdc99556ed41cb22
Fernando Mumbach <fermuch@ayvu.net>
2025-07-31 08:08:17 -0300

    compilable driver

--- package/imx-board-wlan/imx-board-wlan.mk
+++ package/imx-board-wlan/imx-board-wlan.mk
@@ -4,8 +4,10 @@
 #
 ################################################################################
 
-IMX_BOARD_WLAN_VERSION = 276713bbe1ec417fabbe91baeedad75bcb3bee31
-IMX_BOARD_WLAN_SITE = https://coral.googlesource.com/imx-board-wlan-src
+# IMX_BOARD_WLAN_VERSION = 276713bbe1ec417fabbe91baeedad75bcb3bee31
+# IMX_BOARD_WLAN_SITE = https://coral.googlesource.com/imx-board-wlan-src
+IMX_BOARD_WLAN_VERSION = 7dc91e5977f31d60741c55682564788c0f930163
+IMX_BOARD_WLAN_SITE = https://github.com/nxp-imx/qcacld-2.0-imx.git
 IMX_BOARD_WLAN_SITE_METHOD = git
 IMX_BOARD_WLAN_LICENSE = GPL-2.0
 IMX_BOARD_WLAN_LICENSE_FILES = LICENSE
@@ -18,7 +20,6 @@ define IMX_BOARD_WLAN_BUILD_CMDS
 		CROSS_COMPILE=$(TARGET_CROSS) \
 		WLAN_ROOT=$(@D) \
 		MODNAME=wlan \
-		CONFIG_QCA_CLD_WLAN=m \
 		modules
 endef
 
